76e64a3c712e67ba51fa79dfc9f22c6f50aed3bb
Kapil Garg <kgarg6@sapient.com>
2016-03-14 18:11:32 -0500

    added lactose/gluten-free dicts and code formatting

--- get_db_data.py
+++ get_db_data.py
@@ -1,92 +1,96 @@
-""" Script to pull data from JSON DBs into python objects """
+""" Script to pull data from JSON DBs into python objects"""
 import json
 import io
 
 def get_db_cooking_methods():
+    with io.open("data/cooking-methods_db.json") as datafile:
+        data = json.load(datafile)
 
-	with io.open("data/cooking-methods_db.json") as datafile:
-		data = json.load(datafile)
+    return [method for method in data["cooking-methods"]]
 
-	return [method for method in data["cooking-methods"]]
+class CookingTool(object):
+    def __init__(self, name="", alternatives=None, verbs=None):
+        # assign value to class variables
+        self.name = name
 
-class CookingTool():
+        if alternatives is None:
+            self.alternatives = []
+        else:
+            self.alternatives = alternatives
 
-	def __init__(self, name="", alternatives=[], verbs=[]):
-		self.name = name
-		self.alternatives = alternatives
-		self.verbs = verbs
+        if verbs is None:
+            self.verbs = []
+        else:
+            self.verbs = verbs
 
-	def get_name(self):
-		return self.name
+    def get_name(self):
+        return self.name
 
-	def get_alternatives(self):
-		return self.alternatives
+    def get_alternatives(self):
+        return self.alternatives
 
-	def get_verbs(self):
-		return self.verbs
+    def get_verbs(self):
+        return self.verbs
 
 def get_db_cooking_tools():
+    with io.open("data/cooking-tools_db.json") as datafile:
+        data = json.load(datafile)
 
-	with io.open("data/cooking-tools_db.json") as datafile:
-		data = json.load(datafile)
+    json_list = data["cooking-tools"]
+    cooking_tools = []
+    for tool in json_list:
+        name = tool.keys()[0]
+        alternatives = tool[name]["alternatives"]
+        verbs = tool[name]["verbs"]
+        cooking_tools.append(CookingTool(name, alternatives, verbs))
 
-	json_list = data["cooking-tools"]
-	cooking_tools = []
-	for tool in json_list:
-		name = tool.keys()[0]
-		alternatives = tool[name]["alternatives"]
-		verbs = tool[name]["verbs"]
-		cooking_tools.append(CookingTool(name, alternatives, verbs))
-
-	return cooking_tools
+    return cooking_tools
 
 def get_db_cooking_verbs():
+    with io.open("data/cooking-verbs_db.json") as datafile:
+        data = json.load(datafile)
 
-	with io.open("data/cooking-verbs_db.json") as datafile:
-		data = json.load(datafile)
-
-	return [verb for verb in data["cooking-verbs"]]
+    return [verb for verb in data["cooking-verbs"]]
 
 def get_db_transforms():
+    with io.open("data/diet-transform_db.json") as datafile:
+        data = json.load(datafile)
 
-	with io.open("data/diet-transform_db.json") as datafile:
-		data = json.load(datafile)
+    transform_dict = data["diet-transforms"]
 
-	transformDict = data["diet-transforms"]
+    to_omnivore = transform_dict["toOmnivore"]
+    to_pescatarian = transform_dict["toPescatarian"]
+    to_vegetarian = transform_dict["toVegetarian"]
+    to_vegan = transform_dict["toVegan"]
+    to_vegan_animal_products = transform_dict["toVeganAnimalProducts"]
+    to_lactose_free = transform_dict["toLactoseFree"]
+    to_gluten_free = transform_dict["toGlutenFree"]
 
-	toOmnivore = transformDict["toOmnivore"]
-	toPescatarian = transformDict["toPescatarian"]
-	toVegetarian = transformDict["toVegetarian"]
-	toVegan = transformDict["toVegan"]
-	toVeganAnimalProducts = transformDict["toVeganAnimalProducts"]
-	return (toOmnivore, toPescatarian, toVegetarian, toVegan, toVeganAnimalProducts)
+    return (to_omnivore, to_pescatarian, to_vegetarian, to_vegan, to_vegan_animal_products, \
+            to_lactose_free, to_gluten_free)
 
 def get_db_food_textures():
+    with io.open("data/food-textures_db.json") as datafile:
+        data = json.load(datafile)
 
-	with io.open("data/food-textures_db.json") as datafile:
-		data = json.load(datafile)
+    texture_dict = data["food-textures"]
 
-	textureDict = data["food-textures"]
+    fish_textures = texture_dict["fish"]
+    meat_textures = texture_dict["meat"]
+    plant_textures = texture_dict["plantBased"]
 
-	fishTextures = textureDict["fish"]
-	meatTextures = textureDict["meat"]
-	plantTextures = textureDict["plantBased"]
-
-	return (meatTextures, fishTextures, plantTextures)
+    return (meat_textures, fish_textures, plant_textures)
 
 def main():
+    # cooking_methods = get_db_cooking_methods()
+    # cooking_tools = get_db_cooking_tools()
+    # cooking_verbs = get_db_cooking_verbs()
 
-	COOKING_METHODS = get_db_cooking_methods()
+    # to_omnivore, to_pescatarian, to_vegetarian, to_vegan, to_vegan_animal_products, \
+    # to_lactose_free, to_gluten_free = get_db_transforms()
 
-	COOKING_TOOLS = get_db_cooking_tools()
-
-	COOKING_VERBS = get_db_cooking_verbs()
-
-	TO_OMNIVORE, TO_PESCATARIAN, TO_VEGETARIAN, TO_VEGAN, TO_VEGAN_ANIMAL_PRODUCTS = get_db_transforms()
-
-	MEAT_TEXTURES, FISH_TEXTURES, PLANT_TEXTURES = get_db_food_textures()
-
-	return
+    # meat_textures, fish_textures, plant_textures = get_db_food_textures()
+    return
 
 if __name__ == '__main__':
     main()
